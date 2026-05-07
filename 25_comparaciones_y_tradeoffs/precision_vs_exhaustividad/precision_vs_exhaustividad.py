80911259c9b61638c9fda7d27a716cf81af1a13b
FigmaPrompt <ben.kemp@lexiconredesneuronales.com>
2026-05-07 13:57:39 +0200

    Commit

---
+++ 25_comparaciones_y_tradeoffs/precision_vs_exhaustividad/precision_vs_exhaustividad.py
@@ -0,0 +1,7 @@
+from sklearn.metrics import precision_score, recall_score
+
+y_true = [1, 0, 1, 1]
+y_pred = [1, 0, 0, 1]
+
+print(precision_score(y_true, y_pred))
+print(recall_score(y_true, y_pred))
---
+++ 25_comparaciones_y_tradeoffs/precision_vs_exhaustividad/precision_vs_exhaustividad_pytorch.py
@@ -0,0 +1,1 @@
+# calcular VP, FP y FN manualmente
